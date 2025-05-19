659bc02957723e8636adfe612eb3d9e3fb892013
Joesanjo <22bcae34@kristujayanti.com>
2025-05-19 10:25:47 +0530

    Operation Changed

--- python.py
+++ python.py
@@ -2,8 +2,8 @@
 num1 = int(input('Enter first number: '))
 num2 = int(input ('Enter second number: '))
 
-# multiply two numbers
-sum= float(num1 * num2)
+# add two numbers
+sum= float(num1 + num2)
 
 # Display the sum
 print(f'The sum of {num1} and {num2} is {sum}')
